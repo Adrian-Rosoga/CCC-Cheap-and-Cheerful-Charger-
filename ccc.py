8b014ee31c8d3c584982e679110241ca75f3330a
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2020-06-23 18:48:43 +0100

    Another leanup

--- ccc.py
+++ ccc.py
@@ -5,13 +5,11 @@
 v1 - 26 Jan 2018
     # Support for TP-Link HS100 charger
 v2 - 31 Dec 2019
-    # Refactoring, encapsulate logic in relay specific class
+    # Refactoring, encapsulate logic in Switch specific class
 
 TODO
     # Ensure only one instance could run
-    # run() instead of popen()
     # requests instead of urllib
-    # 
 '''
 
 
@@ -60,22 +58,18 @@ MAX_ALERT_CHARGE = MAX_CHARGE + 5
 MIN_ALERT_CHARGE = MIN_CHARGE - 5
 
 
-@unique
-class RelayState(Enum):
-    ON = auto()
-    OFF = auto()
-    NA = auto()
+Switch = None
 
 
 def wifi_ssid() -> str:
     """Wifi ssid or empty string if not connected to wifi"""
 
-    if not IS_WINDOWS:
-        popen = subprocess.Popen(['iwgetid', '-r'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, _ = popen.communicate()
+    if IS_WINDOWS:
+        cmd_list = ['netsh', 'wlan', 'show', 'interfaces']
     else:
-        popen = subprocess.Popen(['netsh', 'wlan', 'show', 'interfaces'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, _ = popen.communicate()
+        cmd_list = ['iwgetid', '-r']
+
+    output = subprocess.check_output(cmd_list)
 
     return output.decode()
 
@@ -97,41 +91,48 @@ def beep(frequency=2500, duration_msec=1000):
         playsound('beep-low-freq.wav')
 
 
-class Relay(ABC):
+class Switch(ABC):
+
+    @unique
+    class State(Enum):
+        ON = auto()
+        OFF = auto()
+        NA = auto()
 
     @property
     @abstractmethod
     def state(self):
-        return RelayState.NA
+        pass
 
     @abstractmethod
-    def turn_power(self, on_off):
+    def turn_on(self):
+        pass
+
+    @abstractmethod
+    def turn_off(self):
         pass
 
 
-class HIDRelay(Relay):
+class HIDSwitch(Switch):
 
     @property
     def state(self):
-
-        popen = subprocess.Popen(['hidusb-relay-cmd.exe', 'state'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, _ = popen.communicate()
-        if 'R1=ON' in output.decode("utf-8"):
-            return RelayState.ON
-        elif 'R1=OFF' in output.decode("utf-8"):
-            return RelayState.OFF
+        output = subprocess.check_output(['hidusb-Switch-cmd.exe', 'state'])
+        if 'R1=ON' in output.decode():
+            return Switch.State.ON
+        elif 'R1=OFF' in output.decode():
+            return Switch.State.OFF
         else:
-            return RelayState.NA
+            return Switch.State.NA
 
-    def turn_power(self, on_off):
+    def turn_on(self):
+        os.system('hidusb-relay-cmd.exe on 1')
 
-        if on_off:
-            os.system('hidusb-relay-cmd.exe on 1')
-        else:
-            os.system('hidusb-relay-cmd.exe off 1')
+    def turn_off(self):
+        os.system('hidusb-relay-cmd.exe off 1')
 
 
-class HS100Relay(Relay):
+class HS100Switch(Switch):
 
     @property
     def state(self):
@@ -139,52 +140,53 @@ class HS100Relay(Relay):
         try:
             output = urllib.request.urlopen("http://192.168.1.103/status", timeout=TIMEOUT).read()
             data = json.loads(output)
-            #print(output)
+            logging.debug(output)
             if data['system']['get_sysinfo']['state'] is None:
-                return RelayState.NA
-            return RelayState.ON if data['system']['get_sysinfo']['state'] == 1 else RelayState.OFF
+                return Switch.State.NA
+            return Switch.State.ON if data['system']['get_sysinfo']['state'] == 1 else Switch.State.OFF
         except:
-            print('Error: In HS100.state()')
-            return RelayState.NA
+            logging.error('Error: In HS100.state()')
+            return Switch.State.NA
 
-
-    def turn_power(self, on_off):
+    def turn_on(self):
 
         try:
-            if on_off:
-                urllib.request.urlopen("http://192.168.1.103/on", timeout=TIMEOUT)
-            else:
-                urllib.request.urlopen("http://192.168.1.103/off", timeout=TIMEOUT)
+            urllib.request.urlopen("http://192.168.1.103/on", timeout=TIMEOUT)
         except:
-            print('Error: In HS100.turn_power()')
+            logging.error('Exception thrown in HS100.turn_power()')
+
+    def turn_off(self):
+
+        try:
+            urllib.request.urlopen("http://192.168.1.103/off", timeout=TIMEOUT)
+        except:
+            logging.error('Error: In HS100.turn_power()')
 
 
-class EnergenieRelay(Relay):
-
-    @property
-    def state(self):
-        return RelayState.NA
-
-    def turn_power(self, on_off):
-
-        if on_off:
-            urllib.request.urlopen("http://192.168.1.108:8000/on", timeout=TIMEOUT)
-        else:
-            urllib.request.urlopen("http://192.168.1.108:8000/off", timeout=TIMEOUT)
-
-
-class NoRelay(Relay):
+class EnergenieSwitch(Switch):
 
     @property
     def state(self):
-        return RelayState.NA
+        return Switch.State.NA
 
-    def turn_power(self, on_off):
+    def turn_on(self):
+        urllib.request.urlopen("http://192.168.1.108:8000/on", timeout=TIMEOUT)
+
+    def turn_off(self):
+        urllib.request.urlopen("http://192.168.1.108:8000/off", timeout=TIMEOUT)
+
+
+class NoSwitch(Switch):
+
+    @property
+    def state(self):
+        return Switch.State.NA
+
+    def turn_on(self):
         pass
 
-
-relay = EnergenieRelay()
-#relay = NoRelay()
+    def turn_off(self):
+        pass
 
 
 def power_plugged():
@@ -196,14 +198,14 @@ def turn_power_off():
 
     try:
         logging.info('Program getting killed, turning power off now...')
-        relay.turn_power(False)
+        Switch.turn_power(False)
         logging.info('Program killed, turned power off')
     except urllib.error.URLError:
         logging.error('Caught urllib.error.URLError')
     except OSError:
         logging.error('Caught OSError')
     except Exception:
-        print('TODO: Exception caught in turn_power_off()')
+        logging.error('TODO: Exception caught in turn_power_off()')
         logging.error("Something bad happened", exc_info=True)
 
 
@@ -217,6 +219,10 @@ def battery_percent():
     return psutil.sensors_battery().percent
 
 
+def bool2onoff(value):
+    return 'ON' if value else 'OFF'
+
+
 def control(control=True):
 
     # Hack for manual charging
@@ -225,53 +231,49 @@ def control(control=True):
     elif battery_percent() > MAX_CHARGE_MANUAL and power_plugged():
         beep(2000, 3000)
 
-    logging.info(f'{battery_percent():.1f}% Relay={relay.state} Power={power_plugged()}')
+    logging.info(f'{battery_percent():.1f}% SwitchState={str(switch.state.name)} Power={bool2onoff(power_plugged())}')
 
     if not control:
         return
 
-    if relay.state == RelayState.NA:
-        #return
-        pass
-
     if battery_percent() <= MIN_CHARGE:
 
-        if relay.state == RelayState.OFF or relay.state == 'N/A':
-            relay.turn_power(ON)
+        if switch.state == Switch.State.OFF or switch.state == Switch.State.NA:
+            switch.turn_on()
             logging.info(f'\t{battery_percent():.1f}% - Turn power ON')
 
             # Check power is indeed on - wait for a few secs to give the power the time to reach the
             # computer
             time.sleep(10)
-            if power_plugged() != 'ON':
+            if not power_plugged():
                 logging.error('\t### Not charging although power turned ON')
                 # beep(1000, 1000)  # Get rid of the pesky 2 beeps
                 playsound('Battery_Alert.wav')
 
         # Turn power ON anyway to guard if the above command failed
-        relay.turn_power(ON)
+        switch.turn_on()
 
     elif battery_percent() >= MAX_CHARGE:
 
-        if relay.state != 'OFF' or relay.state == RelayState.NA:
-            relay.turn_power(True)
+        if switch.state != Switch.State.OFF or switch.state == Switch.State.NA:
+            switch.turn_on()
             logging.info(f'\t{battery_percent():.1f}% - Turn power OFF')
 
             # Check power is indeed off - wait a few secs
             time.sleep(10)
             if power_plugged():
-                logging.error('\t### Relay stuck on ON position!?')
+                logging.error('\t### Switch stuck on ON position!?')
                 #beep(1000, 1000)
                 playsound('Battery_High.m4a')
 
         # Turn power OFF anyway to guard if the above command failed
-        relay.turn_power(False)
+        switch.turn_off()
 
-    if relay.state == RelayState.OFF and power_plugged() == RelayState.ON:
+    if switch.state == Switch.State.OFF and power_plugged():
         logging.warning('\t### Charging when not supposed to!?')
         beep(1000, 1000)
 
-    if relay.state == RelayState.ON and not power_plugged():
+    if switch.state == Switch.State.ON and not power_plugged():
         logging.warning('\t### Plug charger in or check why not charging!')
         beep(1000, 1000)
 
@@ -279,26 +281,24 @@ def control(control=True):
 def test_on_off():
 
     while True:
-        relay.turn_power(True)
+        switch.turn_on()
         time.sleep(30)
-        relay.turn_power(False)
+        switch.turn_off()
         time.sleep(30)
-
-
-'''
-At logoff:
-wndproc: msg=17 wparam=0 lparam=-2147483648
-wndproc: msg=22 wparam=1 lparam=-2147483648
-'''
 
 
 def wndproc(hwnd, msg, wparam, lparam):
+    '''
+    At logoff:
+    wndproc: msg=17 wparam=0 lparam=-2147483648
+    wndproc: msg=22 wparam=1 lparam=-2147483648
+    '''
 
     #logging.info("wndproc: msg=%s wparam=%s lparam=%s" % (msg, wparam, lparam))
     if (msg == win32con.WM_POWERBROADCAST and wparam == win32con.PBT_APMSUSPEND) or \
        (msg == win32con.WM_ENDSESSION):
 
-        relay.turn_power(OFF)
+        switch.turn_off()
         logging.info(f'\t### {battery_percent():1.f}% - Entering sleep, turned power OFF')
 
 
@@ -334,11 +334,7 @@ def listen_for_sleep():
     except Exception as e:
         logging.info("Exception: %s" % str(e))
 
-    if hwnd is None:
-        logging.info("hwnd is none!")
-    else:
-        logging.info("hwnd=%s" % hwnd)
-        pass
+    logging.info(f'hwnd={hwnd}')
 
     while True:
         win32gui.PumpWaitingMessages()
@@ -359,19 +355,19 @@ class PowerControlThread(threading.Thread):
             try:
                 control(self.control)
             except HTTPError as e:
-                logging.info('HTTPError: The server couldn\'t fulfill the request')
-                logging.info('\tError code: ' + str(e.code))
+                logging.error('HTTPError: The server couldn\'t fulfill the request')
+                logging.error('\tError code: ' + str(e.code))
                 #beep(600, 2000)
             except URLError as e:
-                logging.info('URLError: We failed to reach a server')
-                logging.info('\tReason: ' + str(e.reason))
+                logging.error('URLError: We failed to reach a server')
+                logging.error('\tReason: ' + str(e.reason))
                 #beep(600, 2000)
             except timeout:
-                logging.info('timeout: socket timed out')
+                logging.error('timeout: socket timed out')
                 #beep(600, 2000)
             except Exception as ex:
-                logging.info('Exception: ' + ex.__class__.__name__)
-                print(ex)
+                logging.error('Exception: ' + ex.__class__.__name__)
+                logging.error(ex)
                 traceback.print_exc(file=sys.stdout)
                 #beep(600, 2000)
 
@@ -390,8 +386,7 @@ class WatchdogThread(threading.Thread):
 
             if battery_percent() >= MAX_ALERT_CHARGE:
                 logging.info(f'\t### Overcharged above {MAX_ALERT_CHARGE:.1f}% - {battery_percent():.1f}%')
-                #if relay.state == 'ON':
-                if psutil.sensors_battery().power_plugged:
+                if power_plugged():
                     beep(500, 3000)
                     time.sleep(0.1)
                     beep(500, 3000)
@@ -400,8 +395,7 @@ class WatchdogThread(threading.Thread):
 
             if battery_percent() <= MIN_ALERT_CHARGE:
                 logging.info(f'\t### Undercharged below {MIN_ALERT_CHARGE:.1f}% - {battery_percent():.1f}%')
-                #if relay.state == 'OFF':
-                if not psutil.sensors_battery().power_plugged:
+                if not power_plugged():
                     beep(500, 3000)
 
             time.sleep(60)
@@ -417,13 +411,13 @@ class SingleInstanceThread(threading.Thread):
 
         if IS_WINDOWS:
 
-            print("Checking if another instance is running...")
+            logging.info("Checking if another instance is running...")
 
             mutex = win32event.CreateMutex(None, False, 'ccc')
             last_error = win32api.GetLastError()
 
             if last_error == ERROR_ALREADY_EXISTS:
-                print('Another instance already running. Exiting.')
+                logging.info('Another instance already running. Exiting.')
                 os._exit(1)
 
             while True:
@@ -443,28 +437,27 @@ class SleepThread(threading.Thread):
 
         while True:
 
-            popen = subprocess.Popen(['xprintidle'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output, _ = popen.communicate()
+            output = subprocess.check_output(['xprintidle'])
 
-            inactivity_secs = int(output)//1000
+            inactivity_secs = int(output.decode()) // 1000
 
-            print(f'inactivity_secs={inactivity_secs}')
+            logging.info(f'inactivity_secs={inactivity_secs}')
 
             if inactivity_secs >= SLEEP_AFTER_SECS:
 
-                print('Turning power off and going to sleep...')
+                logging.info('Turning power off and going to sleep...')
 
                 try:
-                    relay.turn_power(OFF)
+                    switch.turn_off()
                 except:
-                    print('Error: Exception thrown when calling relay.turn_power(OFF)')
+                    logging.error('Exception thrown when calling Switch.turn_power(False)')
                 
                 time.sleep(5)
                 os.system('systemctl suspend')
 
             else:
 
-                print('Activity detected, not going to sleep!')
+                logging.info('Activity detected, not going to sleep!')
 
             time.sleep(SLEEP_AFTER_SECS + 10)
 
@@ -482,10 +475,15 @@ def main():
     parser.add_argument('--nocontrol', help='no power control, just monitor', action='store_true')
     args = parser.parse_args()
 
+    global switch
+
+    switch = EnergenieSwitch()
+
     no_control = True if args.nocontrol else False
     control = not no_control
 
     if no_control:
+        switch=NoSwitch()
         logging.info('Monitoring mode, power source not controlled')
 
     sys.stderr = sys.stdout
@@ -508,5 +506,4 @@ def main():
 
 
 if __name__ == "__main__":
-
     main()
