077a1d7f9d7f7295c48cffaa0c2cf20cfdcd4947
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2020-09-11 21:32:21 +0100

    Loud beep when past max alert limits

--- ccc.py
+++ ccc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-'''
+"""
 
 v1 - 26 Jan 2018
     # Support for TP-Link HS100 charger
@@ -10,7 +10,7 @@ v2 - 31 Dec 2019
 TODO
     # Ensure only one instance could run
     # requests instead of urllib
-'''
+"""
 
 import time
 import os
@@ -56,8 +56,8 @@ SLEEP_AFTER_INACTIVITY_MINS = 4
 
 MIN_CHARGE, MAX_CHARGE = 45, 55
 MIN_CHARGE_MANUAL, MAX_CHARGE_MANUAL = MIN_CHARGE - 1, MAX_CHARGE + 1
-MAX_ALERT_CHARGE = MAX_CHARGE + 5
-MIN_ALERT_CHARGE = MIN_CHARGE - 5
+MAX_ALERT_CHARGE = 80
+MIN_ALERT_CHARGE = 20
 
 TIMEOUT = 10
 START_QUIET_TIME = datetime.time(20, 0)
@@ -84,11 +84,10 @@ def should_be_quiet() -> bool:
 
 
 def is_time_between(begin_time, end_time, check_time=None):
-    # If check time is not given, default to current UTC time
     check_time = check_time or datetime.datetime.now().time()
     if begin_time < end_time:
-        return check_time >= begin_time and check_time <= end_time
-    else: # crosses midnight
+        return begin_time <= check_time <= end_time
+    else:
         return check_time >= begin_time or check_time <= end_time
 
 
@@ -106,6 +105,17 @@ def beep(frequency=2500, duration_msec=1000):
         winsound.Beep(frequency, duration_msec)
     else:
         playsound('beep-low-freq.wav')
+
+
+def beep_loud(frequency=2500, duration_msec=3000):
+
+    if should_be_quiet():
+        return
+
+    if IS_WINDOWS:
+        winsound.Beep(frequency, duration_msec)
+    else:
+        playsound('beep-loud.wav')
 
 
 def power_plugged():
@@ -164,7 +174,7 @@ def control(control=True):
 
         if not power_plugged():
             logging.error('\t### Not charging')
-            beep(1000, 1000)  # Get rid of the pesky 2 beeps
+            beep(1000, 1000)
             voice_alert('Battery_Low_Alert.wav')
 
         # Turn power ON anyway to guard if the above command failed
@@ -183,7 +193,7 @@ def control(control=True):
             time.sleep(10)
             if power_plugged():
                 logging.error('\t### Switch stuck on ON position!?')
-                beep(1000, 1000)
+                beep(1000, 3000)
                 voice_alert('Battery_High_Alert.wav')
 
         # Turn power OFF anyway to guard if the above command failed
@@ -234,10 +244,10 @@ def listen_for_sleep():
                                        0,
                                        hinst,
                                        None)
+        logging.info(f'hwnd={hwnd}')
     except Exception as e:
         logging.error(f'Exception caught: {str(e)}')
-
-    logging.info(f'hwnd={hwnd}')
+        return
 
     while True:
         win32gui.PumpWaitingMessages()
@@ -280,17 +290,17 @@ class WatchdogThread(threading.Thread):
 
             battery_level = battery_percent()
             if battery_level >= MAX_ALERT_CHARGE:
-                logging.info(f'\t### Overcharged above {MAX_ALERT_CHARGE:.1f}% - {battery_level:.1f}%')
+                logging.info(f'\t### Overcharged above {MAX_ALERT_CHARGE:.1f}% - at {battery_level:.1f}%')
                 if power_plugged():
-                    beep(500, 3000)
+                    beep_loud(500, 3000)
                     time.sleep(0.1)
-                    beep(500, 3000)
+                    beep_loud(500, 3000)
                     time.sleep(0.1)
-                    beep(500, 3000)
+                    beep_loud(500, 3000)
             elif battery_level <= MIN_ALERT_CHARGE:
-                logging.info(f'\t### Undercharged below {MIN_ALERT_CHARGE:.1f}% - {battery_level:.1f}%')
+                logging.info(f'\t### Undercharged below {MIN_ALERT_CHARGE:.1f}% - at {battery_level:.1f}%')
                 if not power_plugged():
-                    beep(500, 3000)
+                    beep_loud(500, 3000)
 
             time.sleep(60)
 
@@ -423,7 +433,7 @@ def main():
 
     sys.stderr = sys.stdout
 
-    # To prevent overcharging tunr power off when killing the program
+    # To prevent overcharging turn power off when killing the program
     if not IS_WINDOWS:
         atexit.register(turn_power_off)
         signal.signal(signal.SIGUSR1, handler)
@@ -432,8 +442,7 @@ def main():
 
     PowerControlThread(control).start()
 
-    if False:
-        WatchdogThread().start()
+    WatchdogThread().start()
 
     if sleep_on_inactivity:
         if not IS_WINDOWS:
